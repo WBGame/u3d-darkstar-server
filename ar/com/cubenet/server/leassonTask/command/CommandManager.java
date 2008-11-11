daa9ae39805c691cf86fc2108ae138a0b2a11a24
sebastianperruolo <sebastianperruolo@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2008-11-11 20:28:19 +0000

    Tarea id 9 La idea de la gente de diseño y que me parece bastante razonable es que todo lo que son tareas del servidor se crearan vía instancias de la interface Task que provee darkstar y hacer uso del task manager para agregalas a la cola de ejecución. Necesito que hagas código que según algún mensaje del cliente por ejemplo cree instancias de un Task especifico y muestres como se ejecuta en "background". Se que funcionan del mismo modo que los Thread nativos en java, pero necesito que hagas algo de codigo para verlo mas concretamente y si lo consideras necesario hacete un gdoc con la explicación porque probablemente todos vamos a tener que trabajar con eso.

---
+++ ar/com/cubenet/server/leassonTask/command/CommandManager.java
@@ -0,0 +1,36 @@
+package ar.com.cubenet.server.leassonTask.command;
+
+import ar.com.cubenet.server.leassonTask.tasks.BroadcastLoginCommandTask;
+import ar.com.cubenet.server.leassonTask.tasks.LeaveChannelCommandTask;
+import ar.com.cubenet.server.leassonTask.tasks.UnknowCommandTask;
+import ar.com.cubenet.server.leassonTask.tasks.WhoAmICommandTask;
+
+import com.sun.sgs.app.AppContext;
+import com.sun.sgs.app.ClientSession;
+import com.sun.sgs.app.Task;
+
+public class CommandManager {
+	protected static CommandManager instance = null;
+	protected CommandManager(){
+		
+	}
+	public static CommandManager getInstance(){
+		if(instance==null){
+			instance = new CommandManager();
+		}
+		return instance;
+	}
+	public void process(String msj, ClientSession session, String channelName){
+		Task task = null;
+		if(msj.equals("/whoami")){
+			task = new WhoAmICommandTask(session);
+		}else if(msj.equals("/login")){
+			task = new BroadcastLoginCommandTask(session);
+		}else if(msj.equals("/leave")){
+			task = new LeaveChannelCommandTask(session, channelName);
+		}else{
+			task = new UnknowCommandTask(session, msj);
+		}
+		if(task!=null) AppContext.getTaskManager().scheduleTask(task);
+	}
+}
