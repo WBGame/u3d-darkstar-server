10e8b62a6c8e8e37569233dd828fa4fe20f5fe23
sebastianperruolo <sebastianperruolo@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2008-12-03 00:07:40 +0000

    Se corrige el estilo del código fuente

--- UD3Server/src/main/java/ar/edu/unicen/exa/server/grid/IGridStructure.java
+++ UD3Server/src/main/java/ar/edu/unicen/exa/server/grid/IGridStructure.java
@@ -1,59 +1,82 @@
-/**
- * 
- */
-package server.grid;
+package ar.edu.unicen.exa.server.grid;
 
 import java.io.Serializable;
 import com.sun.sgs.app.ManagedObject;
 import com.jme.math.Vector3f;
 
 /** 
- *  Es una coleccion de celdas dispuestas de alguna manera con el objetivo de particionar el espacio fisico del mundo al cual representa. La estructura debe poseer un identificador unico que se corresponde uno a uno con el identificador del  {@code IGameState} del cliente. Es decir, representa a un objeto  {@code IGameState} del cliente en el servidor.
- * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ *  Es una coleccion de celdas dispuestas de alguna manera con el objetivo de
+ *  particionar el espacio fisico del mundo al cual representa. La estructura
+ *  debe poseer un identificador unico que se corresponde uno a uno con el
+ *  identificador del  {@code IGameState} del cliente. Es decir, representa
+ *  a un objeto  {@code IGameState} del cliente en el servidor.
+ *  
+ * @generated "De UML a Java V5.0 
+ * 		(com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public interface IGridStructure extends Serializable, ManagedObject {
 	/**
 	 * Retorna el identificador del mundo.
-	 * @return 
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 * @return el identificador del mundo. 
+	 * @generated "De UML a Java V5.0 
+	 * 		(com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public String getIdWorld();
+	String getIdWorld();
 
 	/**
-	 * Establece el identificador del mundo. Dicho identificador no debe repetirse entre las estructuras.
-	 * @param id 
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 * Establece el identificador del mundo. Dicho identificador no debe
+	 * repetirse entre las estructuras.
+	 * 
+	 * @param id identificador del mundo
+	 * @generated "De UML a Java V5.0 
+	 * 		(com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public void setIdWorld(String id);
+	void setIdWorld(String id);
 
 	/**
-	 * Retorna la   {@link Cell} que corresponde a la posicion pasada por parametro.
-	 * @return 
-	 * @param position 
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 * Retorna la {@link Cell} que corresponde a la posicion pasada por 
+	 * parámetro.
+	 * 
+	 * @return la {@link Cell} que corresponde
+	 * @param position posición a evaluar
+	 * @generated "De UML a Java V5.0 
+	 * 		(com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public Cell getCell(Vector3f position);
+	Cell getCell(Vector3f position);
 
 	/**
-	 * Retorna la celda inicial, es decir la celda en la que aparece por defecto el jugador cuando ingresa por primera vez al mundo.
-	 * @return 
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 * Retorna la celda inicial, es decir la celda en la que aparece por
+	 * defecto el jugador cuando ingresa por primera vez al mundo.
+	 * 
+	 * @return la celda inicial
+	 * @generated "De UML a Java V5.0 
+	 * 		(com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public Cell getSpawnCell();
+	Cell getSpawnCell();
 
 	/**
-	 * Retorna la coleccion de aquellas celdas que son adyacentes a la celda pasada por parametro. Se debe proporcionar tambien la posicion del jugador, para poder determinar la zona dentro de la celda en la que se encuentra, ya que puede influir en la determinacion de cuales son las celdas adyacentes. Ver documentacion del dise�o sobre tratamiento de celdas para mas informacion.
-	 * @return 
-	 * @param cell 
-	 * @param position 
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 * Retorna la coleccion de aquellas celdas que son adyacentes a la celda
+	 * pasada por parametro. Se debe proporcionar tambien la posicion del
+	 * jugador, para poder determinar la zona dentro de la celda en la que se
+	 * encuentra, ya que puede influir en la determinacion de cuales son las
+	 * celdas adyacentes. Ver documentacion del diseñoo sobre tratamiento de
+	 * celdas para mas informacion.
+	 * 
+	 * @return las celdas adyacentes.
+	 * @param cell celda de la cual obtener los adyacentes.
+	 * @param position posición dentro de la celda.
+	 * @generated "De UML a Java V5.0 
+	 * 		(com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public Cell[] getAdjacents(Cell cell, Vector3f position);
+	Cell[] getAdjacents(Cell cell, Vector3f position);
 
 	/**
-	 * Establece la celda inicial, es decir la celda en la que aparece por defecto el jugador cuando ingresa por primera vez al mundo.
-	 * @param spawnCell 
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 * Establece la celda inicial, es decir la celda en la que aparece por
+	 * defecto el jugador cuando ingresa por primera vez al mundo.
+	 * 
+	 * @param spawnCell celda inicial
+	 * @generated "De UML a Java V5.0
+	 * 		(com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public void setSpawnCell(Cell spawnCell);
+	void setSpawnCell(Cell spawnCell);
 }
