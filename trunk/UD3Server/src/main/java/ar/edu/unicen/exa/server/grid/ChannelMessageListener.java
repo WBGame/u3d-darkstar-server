40dbdea9a35d1c1992729a4335e22b0585dc7baf
sebastianperruolo <sebastianperruolo@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2008-12-05 22:57:35 +0000

    Implementación de celda, grilla, matriz de grillas y test.

--- trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/grid/ChannelMessageListener.java
+++ trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/grid/ChannelMessageListener.java
@@ -1,31 +1,48 @@
-/**
- * 
- */
 package ar.edu.unicen.exa.server.grid;
 
-import com.sun.sgs.app.ChannelListener;
-import ar.edu.unicen.exa.server.communication.processors.ServerMsgProcessor;
-import ar.edu.unicen.exa.server.communication.tasks.TaskCommFactory;
-import com.sun.sgs.app.Channel;
-import com.sun.sgs.app.ClientSession;
 import java.nio.ByteBuffer;
 
-/** 
- *  Procesa los mensajes que llegan por los canales. Para esto debe generar un  {@code IMessage} dado los datos recibidos y generar un  {@code IProcessor} que procese el mensaje. No necesariamente debe haber un Listener para cada Channel, por ejemplo se puede asociar el mismo ChannelMessageListener a todos los canales.
- * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+import ar.edu.unicen.exa.server.communication.tasks.TaskCommFactory;
+import ar.edu.unicen.exa.server.communication.tasks.TaskCommunication;
+
+import com.sun.sgs.app.AppContext;
+import com.sun.sgs.app.Channel;
+import com.sun.sgs.app.ChannelListener;
+import com.sun.sgs.app.ClientSession;
+import common.messages.IMessage;
+import common.messages.MessageFactory;
+
+/**
+ * Procesa los mensajes que llegan por los canales. Para esto debe generar un
+ * {@code IMessage} dado los datos recibidos y generar un {@code IProcessor} que
+ * procese el mensaje. No necesariamente debe haber un Listener para cada
+ * Channel, por ejemplo se puede asociar el mismo ChannelMessageListener a todos
+ * los canales.
+ * 
+ * @generated "De UML a Java V5.0
+ *            (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class ChannelMessageListener implements ChannelListener {
 	/**
 	 * @param channel
+	 *            a channel
 	 * @param session
+	 *            the sending client session
 	 * @param msg
-	 * @generated "De UML a Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 *            a message
+	 * @generated "De UML a Java V5.0
+	 *            (com.ibm.xtools.transform.uml2.java5.internal
+	 *            .UML2JavaTransform)"
 	 */
-	public void receivedMessage(Channel channel, ClientSession session,
-			ByteBuffer msg) {
-		// begin-user-code
-		// TODO Apéndice de método generado automáticamente
-
-		// end-user-code
+	public final void receivedMessage(final Channel channel,
+			final ClientSession session, final ByteBuffer msg) {
+		try {
+			IMessage iMessage = MessageFactory.getInstance().createMessage(msg);
+			TaskCommunication taskCommunication = TaskCommFactory.getInstance()
+					.createComTask(iMessage);
+			AppContext.getTaskManager().scheduleTask(taskCommunication);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
