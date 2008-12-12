124e5e5ac5e770e00d55bf2e4325508dc7bd841a
Jair.Tabares <Jair.Tabares@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2008-12-12 04:20:52 +0000

    complete 80% of all the assigned task.

--- trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/communication/tasks/TRotate.java
+++ trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/communication/tasks/TRotate.java
@@ -3,6 +3,10 @@
  */
 package ar.edu.unicen.exa.server.communication.tasks;
 
+import ar.edu.unicen.exa.server.grid.Cell;
+import ar.edu.unicen.exa.server.grid.GridManager;
+import ar.edu.unicen.exa.server.grid.IGridStructure;
+import ar.edu.unicen.exa.server.player.Player;
 import common.messages.IMessage;
 import common.messages.notify.MsgRotate;
 
@@ -34,7 +38,56 @@ public class TRotate extends TaskCommunication {
 	}
 	
 	public void run() {
+		String strNewWorld;
+		String msgReport;
+		strNewWorld = new String();
+		msgReport = new String();
+		//FIXME handle exception and common errors
+		if (!MsgTypes.MSG_MOVE_SEND_TYPE.equals(getMsgType())) {
+			//throw El mensaje no me sirve para esta tarea!
+		    msgReport ="Message Usseless for this taks!";
+			System.err.println(msgReport);
+		}
 		
+		//if is a MsgMove
+		MsgMove msg = (MsgMove) getMessage();
+
+		String userId = msg.getIdDynamicEntity();
+		Player player;
+		try {
+			player = (Player) AppContext.getDataManager()
+					.getBinding(userId);
+		} catch (Exception e) {
+			//TODO Create exception if player {@link userId} cannot be found
+			msgReport ="User <" + userId + "> Cannot be found";
+			System.err.println(msgReport);
+		}
+		//Obtain the IIGridStructure for the player
+		IGridStructure structure = GridManager.getInstance()
+				.getStructure(player.getActualWorld());
+
+		//Obtain the actual palyer cell
+		Cell current = structure.getCell(msg.getPosOrigen());
+		if (current == null) {
+			//TODO Create exception if detect player outside the board \
+			msgReport ="Player outside the board";
+			System.err.println(msgReport);
+		}
+		ClientSession session = player.getSession();
+		current.send(msg, session);
+		Cell[] adyacentes = structure
+							.getAdjacents(current, msg.getPosDestino());
+		
+		//verify the adjacents
+		if (adyacentes == null) {
+			return;
+		}
+		if (adyacentes.length == 0) {
+			return;
+		} 
+	    //TODO calculate the rotation
+		
+	}		
 	}
 	
 }
