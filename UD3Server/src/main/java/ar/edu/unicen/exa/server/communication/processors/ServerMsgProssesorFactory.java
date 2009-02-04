a4c4c15762889f555343d0d249257a113d248c90
sebastianperruolo <sebastianperruolo@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2009-02-04 22:21:54 +0000

    fix al problema de persistencia en la clase ServerMsgProcessor

---
+++ UD3Server/src/main/java/ar/edu/unicen/exa/server/communication/processors/ServerMsgProssesorFactory.java
@@ -0,0 +1,81 @@
+package ar.edu.unicen.exa.server.communication.processors;
+
+
+import common.messages.MsgTypes;
+import common.processors.MsgProcessorFactory;
+
+public class ServerMsgProssesorFactory {
+	private static MsgProcessorFactory INSTANCE = null;
+	
+	private ServerMsgProssesorFactory(){}
+	
+	public static MsgProcessorFactory getInstance(){
+		if(INSTANCE == null){
+			configureMsgProcessorFactory();
+			INSTANCE = MsgProcessorFactory.getInstance();
+		}
+		return INSTANCE;
+	}
+	/**
+	 * Con figura la fabrica de procesadores {@link MsgProcessorFactory}
+	 * asignado para cada tipo de mensaje del framework que es recibido en el
+	 * servidor, un instancia del procesador correspondiente.
+	 */
+	private static void configureMsgProcessorFactory() {
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_ABORT_QUEST_TYPE, new PAbortQuest());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_ADD_2D_GAME_SCORE_TYPE, new PAdd2DGameScore());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_BUY_GAME_TYPE, new PBuyGame());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_FINISH_QUEST_TYPE, new PFinishQuest());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_GET_2DGAME_PRICE_TYPE, new PGet2DGamePrice());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_GET_AVAILABLE_2DGAMES_TYPE,
+				new PGetAvailable2DGames());
+
+		MsgProcessorFactory.getInstance()
+		.addProcessor(MsgTypes.MSG_GET_BUYABLE_2DGAMES_TYPE,
+				new PGetBuyable2DGames());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_GET_PLAYER_TYPE, new PGetPlayer());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_GET_RANKING_TYPE, new PGetRanking());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_GET_TIMES_PLAYED_TYPE, new PGetTimesPlayed());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_START_QUEST_TYPE, new PStartQuest());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_GET_DYNAMIC_ENTITY_TYPE, new PTaskDelegator());
+		
+		//XXX cambio de new PTaskDelegator() a new PEnterWorld()
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_ENTER_WORLD_TYPE, new PEnterWorld());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_MOVE_SEND_TYPE, new PTaskDelegator());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_ROTATE_SEND_TYPE, new PTaskDelegator());
+
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_CHANGE_PLAYER_STATE_SEND_TYPE,
+				new PTaskDelegator());
+		//XXX linea agregada para la tarea perteneciente al cambio de mundo
+		MsgProcessorFactory.getInstance().addProcessor(
+				MsgTypes.MSG_CHANGE_WORLD_TYPE, new PTaskDelegator());
+	}
+}
