fd66a5aa0b6d1d0ce5f71b5f53719eb1182113d7
inchausti.pablo@gmail.com <inchausti.pablo@gmail.com@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2009-02-07 19:01:02 +0000

    Implementacion de esta tarea. Faltaria hacer el cliente para testearla.

--- trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/communication/tasks/TGetDynamicEntity.java
+++ trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/communication/tasks/TGetDynamicEntity.java
@@ -1,6 +1,15 @@
 package ar.edu.unicen.exa.server.communication.tasks;
 
+import ar.edu.unicen.exa.server.entity.DynamicEntity;
+import ar.edu.unicen.exa.server.serverLogic.ModelAccess;
+import com.sun.sgs.app.AppContext;
+import common.exceptions.UnsopportedMessageException;
 import common.messages.IMessage;
+import common.messages.MessageFactory;
+import common.messages.MsgPlainText;
+import common.messages.MsgTypes;
+import common.messages.responses.MsgGetDynamicEntityResponse;
+
 
 /**
  * La tarea se ejecutará al recibir un mensaje ({@link MsgGetDynamicEntity})
@@ -30,21 +39,57 @@ public final class TGetDynamicEntity extends TaskCommunication {
 	}
 	
 	/**
-	 * TODO hacer javaDoc.
+	 * Crear y devuelve una instancia de la clase.
 	 * 
-	 * @param msg the msg
-	 * 
-	 * @return the task communication
+	 * @param msg El mensaje con el que trabajará la tarea internamente.
+	 * @return Una instancia de esta clase
 	 */
 	@Override
 	public TaskCommunication factoryMethod(final IMessage msg) {
 		return new TGetDynamicEntity(msg);
 	}
+	
 	/**
-	 * TODO.
+	 * Este metodo obtiene una entidad dinamica a partir de su identificador 
+	 * contenido en el mensaje recibido. Ademas crea un mensaje de respuesta
+	 * con la informacion de dicha entidad para luego ser enviada al jugador
+	 * que la solicito.
+	 * 
+	 * @encoding UTF-8
 	 */
 	public void run() {
+				
+		//castear al mensage plano
+		MsgPlainText msg = (MsgPlainText) getMessage();
+
+		//obtener el id de la entidad dinamica
+		String idDynamicEntity = msg.getMsg();
 		
+		//Obtener la entidad dinámica a partir del DataManager
+		DynamicEntity dinamicEntity = (DynamicEntity) 
+			AppContext.getDataManager().getBinding(idDynamicEntity);
+		
+		//crear el mensaje de respuesta
+		MsgGetDynamicEntityResponse msgGDER = null;
+		try {
+			msgGDER = (MsgGetDynamicEntityResponse)
+				MessageFactory.getInstance().createMessage(
+				MsgTypes.MSG_GET_DYNAMIC_ENTITY_RESPONSE_TYPE);
+			
+			//seteo el mensaje con la informacion correspondiente
+			msgGDER.setActualWorld(dinamicEntity.getActualWorld());
+			msgGDER.setAngle(dinamicEntity.getAngle());
+			msgGDER.setIdDynamicEntity(dinamicEntity.getIdEntity());
+			msgGDER.setPosition(dinamicEntity.getPosition());
+			msgGDER.setSkin(ModelAccess.getInstance().getSkin(
+					dinamicEntity.getIdEntity()));
+			
+		} catch (UnsopportedMessageException e) {
+			e.printStackTrace();
+		}
+		
+		//enviar el mensaje con la respuesta al Player que solicito
+		getPlayerAssociated().send(msgGDER);
 	}
 	
 }
