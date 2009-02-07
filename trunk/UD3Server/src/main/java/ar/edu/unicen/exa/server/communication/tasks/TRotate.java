0197d02393534bd6bff34c6d800f3503aba45ac1
inchausti.pablo@gmail.com <inchausti.pablo@gmail.com@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2009-02-07 18:06:47 +0000

    Corregi algunas cosas que estaban demas, por ejemplo el envio a las celdas adyacentes, esto no es ncesesario porque cuando el jugador rota nunca cambia de celda, siempre lo hace sobre la celda actual. Fijense quedo re simple y facil de entender.

--- trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/communication/tasks/TRotate.java
+++ trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/communication/tasks/TRotate.java
@@ -2,16 +2,11 @@
 
 package ar.edu.unicen.exa.server.communication.tasks;
 
-import com.sun.sgs.app.AppContext;
 import com.sun.sgs.app.ClientSession;
-
 import ar.edu.unicen.exa.server.grid.Cell;
-import ar.edu.unicen.exa.server.grid.GridManager;
-import ar.edu.unicen.exa.server.grid.IGridStructure;
 import ar.edu.unicen.exa.server.player.Player;
 import common.messages.IMessage;
 import common.messages.MsgTypes;
-//import common.messages.notify.MsgMove;
 import common.messages.notify.MsgRotate;
 
 /**
@@ -51,80 +46,31 @@ public final class TRotate extends TaskCommunication {
 	
 	/**
 	 * Actualizar el angulo de rotacion de la entidad afectada, y reenviar
-	 * el mensaje a travez las celdas pertinentes.
+	 * el mensaje a la celda actual del jugador. No es necesario enviar el 
+	 * mensaje a las celdas adyacentes ya que nunca cambia de celda cuando
+	 * el jugador rota.
 	 */
 	
 	public void run() {
-		String msgReport;
-		msgReport = new String();
-
-		//Ver si el mensaje recibido sea el correspondiente para esta tarea.
-		if (!MsgTypes.MSG_MOVE_SEND_TYPE.equals(getMsgType())) {
-
-			throw new Error("Mensaje no valido para esta tarea");
-		}
-        
 		
-		//Castear al mensage que corresponda.
-		//En este caso casteamos a MsgRotate.
+		//instancia del jugador
+		Player player = getPlayerAssociated();
+		
+		//recuperar la celda actual
+		Cell cell = getCellAssociated();
+  	    		
+		//castear al mensage de rotacion
 		MsgRotate msg = (MsgRotate) getMessage();
-		
-		//El identificador de la entidad que realiza la rotacion.
-		String userId = msg.getIdDynamicEntity();
-		
-		
-		Player player = null;
-		
-		try {
-			player = (Player) AppContext.getDataManager()
-			.getBinding(userId);
-		} catch (Exception e) {
-			//Crea excepcion si el jugador {@link userId} no se puede encontrar.
-			msgReport = "Usuario <" + userId + "> no se puede encontar";
-			System.err.println(msgReport);
-		}
-		
-		//Obtener el IGridStructure para el jugador.
-		IGridStructure structure = GridManager.getInstance()
-		.getStructure(player.getActualWorld());
 
-		//Obtener la celda del mundo actual.
-		Cell current = structure.getCell(player.getPosition());
-		if (current == null) {
-			//TODO Create exception if detect player outside the board.
-			msgReport = "Player outside the board";
-			System.err.println(msgReport);
-		}
-		//Mantener la sesion del cliente.
-		ClientSession session = player.getSession();		
-
-		//Desinscribir al jugador de las celdas adyacentes.
-		Cell[] adyacentes = structure
-		.getAdjacents(current, player.getPosition());
-		for (int i = 0; i < adyacentes.length; i++) {
-			adyacentes[i].leaveFromChannel(session);
-		}
-
-		//Mantiene los nuevos adyacentes despues de la rotacion.
-		current.send(msg, session);
-		adyacentes = structure
-		.getAdjacents(current, msg.getAngle());
-
-		//Verificar los adyacentes
-		if (adyacentes == null) {
-			return;
-		}
-		if (adyacentes.length == 0) {
-			return;
-		} 
-
-	    //Establece el angulo del jugador.
+		//actualizar el angulo del player
 		player.setAngle(msg.getAngle());
 
-		//Y se une a sus canales.
-		for (int i = 0; i < adyacentes.length; i++) {
-			adyacentes[i].joinToChannel(session);
-		}
+		ClientSession session = player.getSession();
 		
+		//crear el mensaje de notificacion
+		msg.setType(MsgTypes.MSG_ROTATE_NOTIFY_TYPE);
+		
+		//reenviar el mendaje de rotacion a la celda actual del jugador 
+		cell.send(msg, session);
 	}		
 }
