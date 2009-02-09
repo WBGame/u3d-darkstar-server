7ee59430517520b3b6b6b177dc8938d8c432ca8b
cabrerafacundo <cabrerafacundo@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2009-02-09 03:09:12 +0000

    Si un player no está suscripto a ningún mundo no es necesario buscar la celda porque ya sabemos que no está en ningúna.

--- UD3Server/src/main/java/ar/edu/unicen/exa/server/player/UserSessionListener.java
+++ UD3Server/src/main/java/ar/edu/unicen/exa/server/player/UserSessionListener.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 import ar.edu.unicen.exa.server.communication.processors.ServerMsgProcessor;
 import ar.edu.unicen.exa.server.communication.processors
-         .ServerMsgProssesorFactory;
+.ServerMsgProssesorFactory;
 import ar.edu.unicen.exa.server.grid.Cell;
 import ar.edu.unicen.exa.server.grid.GridManager;
 import ar.edu.unicen.exa.server.grid.IGridStructure;
@@ -32,7 +32,7 @@ import common.messages.MessageFactory;
  * @encoding UTF-8 
  */
 public final class UserSessionListener 
-		implements ClientSessionListener, Serializable {
+implements ClientSessionListener, Serializable {
 
 	/** The version of the serialized form of this class. */
 	private static final long serialVersionUID = 4348700548351927735L;
@@ -65,17 +65,17 @@ public final class UserSessionListener
 	 */
 	public void setPlayer(final Player player) {
 		if (player == null) {
-            throw new NullPointerException(
-            		"No existe una instancia para la clase Player");
+			throw new NullPointerException(
+					"No existe una instancia para la clase Player");
 		}
-    	DataManager dataMgr = AppContext.getDataManager();
-        try {
-        	this.playerRef = dataMgr.createReference(player);
-        	LOGGER.log(
-            		Level.INFO, "Establecer una referencia al Player: {0} ",
-            		player.getIdEntity()
-            );
-        } catch (Exception e) {
+		DataManager dataMgr = AppContext.getDataManager();
+		try {
+			this.playerRef = dataMgr.createReference(player);
+			LOGGER.log(
+					Level.INFO, "Establecer una referencia al Player: {0} ",
+					player.getIdEntity()
+			);
+		} catch (Exception e) {
 			e.printStackTrace();
 		} 
 	}
@@ -92,26 +92,32 @@ public final class UserSessionListener
 	public void receivedMessage(final ByteBuffer msg) {
 		try {
 			IMessage iMessage = MessageFactory.getInstance().createMessage(msg);
-			
+
 			ServerMsgProcessor processor = 
 				(ServerMsgProcessor) ServerMsgProssesorFactory.getInstance()
 				.createProcessor(iMessage.getType());
-		
+
 			LOGGER.info("Llego mensaje directo al servidor tipo " 
 					+ iMessage.getType());
+
+			// obtener el mundo actual del jugador
+			String actualWorld = getPlayer().getActualWorld();
+
+			// si estoy suscripto a algún mundo => recupero la celda para 
+			// inicializar el proceso
+			if (actualWorld != null) {
+				IGridStructure world = GridManager.getInstance()
+					.getStructure(getPlayer().getActualWorld());
+				Cell cell = world.getCell(getPlayer().getPosition());
+				processor.setCellAssociated(cell);
+			}
 			
-			//obtener el mundo actual del jugador
-			IGridStructure structure = GridManager.getInstance()
-			.getStructure(getPlayer().getActualWorld());
-
-			//obtener la celda donde se encuentra el jugador
-			Cell cell = structure.getCell(getPlayer().getPosition());
-
-			// Inicialización para ejecutar el proceso asociado al mensaje.
+			// inicialización del player
 			processor.setPlayerAssociated(getPlayer());
-			processor.setCellAssociated(cell);
 			
+			// ejecutar el procesador/tarea asociado/a al mensaje recivido.
 			processor.process(iMessage);
+			
 		} catch (MalformedMessageException e) {
 			LOGGER.log(Level.WARNING, "Exception: {0}", e);
 		} catch (UnsopportedMessageException e) {
@@ -128,20 +134,20 @@ public final class UserSessionListener
 	 */
 	public void disconnected(final boolean graceful) {
 		ClientSession session = getPlayer().getSession();
-		
+
 		String grace = null; 
 		if (graceful) {
 			grace = "correctamente";
 		} else {
 			grace = "forzadamente";
 		}
-		
+
 		LOGGER.log(
 				Level.INFO,
 				"El usuario {0} se ha desconectado {1}",
 				new Object[] { session.getName(), grace } 
 		);
-		
+
 		// la sesión es inutilizada para un usuario desconectado. 
 		getPlayer().setSession(null);
 	}
