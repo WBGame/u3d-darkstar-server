57354e08c206c0923f3f2686cdbb954024554f72
robertookopp <robertookopp@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2009-02-12 03:11:01 +0000

    JavaDoc.

--- trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/player/UserSessionListener.java
+++ trunk/UD3Server/src/main/java/ar/edu/unicen/exa/server/player/UserSessionListener.java
@@ -28,7 +28,7 @@ import common.messages.MsgTypes;
 
 /** 
  * Implementación de {@code ClientSessionListener}. 
- * Está asociado uno a uno con cada jugador ( {@link Player} ) del sistema. 
+ * Esta asociado uno a uno con cada {@link Player} del sistema. 
  * Debe atender peticiones de desconexión y mensajes entrantes.
  * 
  * @author Pablo Inchausti <inchausti.pablo at gmail dot com/> 
@@ -37,7 +37,7 @@ import common.messages.MsgTypes;
 public final class UserSessionListener 
 implements ClientSessionListener, Serializable {
 
-	/** The version of the serialized form of this class. */
+	/**  Para cumplir con la version de la clase Serializable. */
 	private static final long serialVersionUID = 4348700548351927735L;
 
 	/** Logger para esta clase. */
@@ -50,21 +50,22 @@ implements ClientSessionListener, Serializable {
 	private ManagedReference<Player> playerRef;
 
 	/**
-	 * Retorna un ( {@link Player} ) a partir de la referencia 
-	 * {@code ManagedReference} al jugador asociado.
+	 * Retorna un {@link Player} a partir de la referencia 
+	 * {@code ManagedReference} al {@link Player} asociado.
 	 * 
-	 * @return Player jugador que hace referencia el {@code ManagedReference}.
+	 * @return Player jugador que hace referencia el {@link ManagedReference}.
 	 */
 	public Player getPlayer() {
 		return playerRef.get();
 	}
 
 	/**
-	 * Establece el {@link Player} para capturar los eventos de este jugador.
+	 * Establece el {@link Player} para capturar los eventos de este 
+	 * {@link Player}.
 	 * Se crea la referencia {@code ManagedReference} al jugador invocando al
 	 * método {@code createReference()} del {@code DataManager}.
 	 * 
-	 * @param player instancia de un jugador.
+	 * @param player instancia de un {@link Player}.
 	 */
 	public void setPlayer(final Player player) {
 		if (player == null) {
@@ -103,19 +104,19 @@ implements ClientSessionListener, Serializable {
 			LOGGER.info("Llego mensaje directo al servidor tipo " 
 					+ iMessage.getType());
 			
-			//obtener el mundo actual del jugador
+			// Obtener el mundo actual del jugador.
 			IGridStructure structure = GridManager.getInstance()
 				.getStructure(getPlayer().getActualWorld());
 			
-			//obtener la celda donde se encuentra el jugador
+			// Obtener la celda donde se encuentra el jugador.
 			Cell cell = structure.getCell(getPlayer().getPosition());
 
-			// inicialización del player
+			// Inicialización del player.
 			processor.setPlayerAssociated(getPlayer());
 
 			processor.setCellAssociated(cell);
 			
-			// ejecutar el procesador/tarea asociado/a al mensaje recivido.
+			// Ejecutar el procesador/tarea asociado/a al mensaje recivido.
 			processor.process(iMessage);
 			
 		} catch (MalformedMessageException e) {
@@ -126,7 +127,7 @@ implements ClientSessionListener, Serializable {
 	}
 
 	/**
-	 * Desconecta al jugador de la sesión que tiene establecida con el 
+	 * Desconecta al {@link Player} de la sesión que tiene establecida con el 
 	 * servidor. 
 	 * 
 	 * @param graceful si {@code true}, el cliente se desconecta
@@ -149,57 +150,57 @@ implements ClientSessionListener, Serializable {
 				new Object[] { session.getName(), grace } 
 		);
 
-		//salir del mundo actual
+		// Salir del mundo actual.
 		exitWorld(player);
 		
-		//la sesión es inutilizada para un usuario desconectado. 
+		// La sesion es inutilizada para un usuario desconectado. 
 		player.setSession(null);
 
 	}
 	
 	/**
-	 * Este metodo se encarga de desuscribir el jugador del canal al que
-	 * esta asociado y enviar el mensaje {@link MsgLeft} a los jugadores
+	 * Este metodo se encarga de desuscribir el {@link Player} del canal al que
+	 * esta asociado y enviar el mensaje {@link MsgLeft} a los {@link Player}s
 	 * que estan en las celdas adyacentes.
 	 * 
-	 * @param player jugador que sale del mundo
+	 * @param player {@link Player} que sale del mundo.
 	 */
 	public void exitWorld(final Player player) {
-		// crear el mensaje de partida del jugador
+		// Crear el mensaje de partida del jugador.
 		IMessage msgLeft = null;
 		try {
 			msgLeft = MessageFactory.getInstance().createMessage(
 					MsgTypes.MSG_LEFT_TYPE);
-			// seteo el id del jugador como mensaje del mismo
+			// Seteo el id del jugador como mensaje del mismo.
 			((MsgPlainText) msgLeft).setMsg(player.getIdEntity());
 		} catch (UnsopportedMessageException e1) {
 			e1.printStackTrace();
 		}
 
-		//obtener el mundo actual del jugador
+		// Obtener el mundo actual del jugador.
 		IGridStructure structure = GridManager.getInstance()
 			.getStructure(player.getActualWorld());
 		
-		//obtener la celda donde se encuentra el jugador
+		// Obtener la celda donde se encuentra el jugador.
 		Cell cell = structure.getCell(player.getPosition());
 		
-		// obtener la sesion del jugador
+		// Obtener la sesion del jugador.
 		ClientSession session = player.getSession();
 		
-		// notificar a la celda actual que la entidad dinamica ha salido
+		// Notificar a la celda actual que la entidad dinamica ha salido.
 		cell.send(msgLeft, session);
 		
-		//obtener los adyacentes de la celda actual
+		// Obtener los adyacentes de la celda actual
 		Cell[] adyacentes = structure
 				.getAdjacents(cell, player.getPosition());
-		//notificar a las celdas adyacentes que el jugador no se encuntra en
-		//la celda
+		// Notificar a las celdas adyacentes que el jugador no se encuentra en
+		// la celda.
 		if (adyacentes != null) {
 			for (int i = 0; i < adyacentes.length; i++) {
 					adyacentes[i].send(msgLeft, session);
 			}
 		} 
-		//desuscribir al jugador de la celda del viejo mundo
+		// Desuscribir al jugador de la celda del viejo mundo.
 		cell.leaveFromChannel(session);
 	}
 }
