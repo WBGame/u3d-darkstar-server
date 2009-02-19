6d19c67d031d81daee43182427197751a7da0410
robertookopp <robertookopp@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2009-02-19 10:49:40 +0000

    Encoding

--- src/main/java/ar/edu/unicen/exa/server/LoginPasswordAuthenticator.java
+++ src/main/java/ar/edu/unicen/exa/server/LoginPasswordAuthenticator.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 
 import javax.security.auth.login.CredentialException;
 
+//import ar.edu.unicen.exa.server.player.Player;
 import ar.edu.unicen.exa.server.serverLogic.ModelAccess;
 
 import com.sun.sgs.auth.IdentityAuthenticator;
@@ -14,16 +15,17 @@ import com.sun.sgs.impl.auth.IdentityImpl;
 import com.sun.sgs.impl.auth.NamePasswordCredentials;
 
 /**
- * Autentificador de usuarios. Para utilizar el autenticador, el cliente 
- * debe implementar el método {@link #getPasswordAuthentication()} para 
+ * Autentificador de usuarios. 
+ * Para utilizar el autenticador, el cliente 
+ * debe implementar el metodo {@link #getPasswordAuthentication()} para 
  * obtener el nombre de usuario y contraseña a travez de la credencial
  * que se utiliza para obtener dicha información. Esta clase permite 
  * validar un usuario a partir de sus datos ingresados contra la 
- * informacion correspondiente por medio del método {@link #checkPlayer()}
+ * informacion correspondiente por medio del metodo {@link #checkPlayer()}
  * de la clase {@link ModelAccess}.
  * 
  * @author Pablo Inchausti &lt;inchausti.pablo at gmail dot com&gt;
- * @encoding UTF-8
+ * @encoding UTF-8.
  */
 public final class LoginPasswordAuthenticator implements IdentityAuthenticator {
 
@@ -32,22 +34,22 @@ public final class LoginPasswordAuthenticator implements IdentityAuthenticator {
 		Logger.getLogger(LoginPasswordAuthenticator.class.getName());
 
 	/**
-	 * Constructor para esta clase. Es necesario aunque no contenga código
-	 * debido a que Darkstar lo requiere para la creación de esta clase.
-	 * @param properties propiedades del auntenticador.
+	 * Constructor para esta clase. Es necesario aunque no contenga codigo,
+	 * debido a que Darkstar lo requiere para la creacion de esta clase.
+	 * @param properties Propiedades del auntenticador.
 	 */
 	public LoginPasswordAuthenticator(final Properties properties) { }
 
 	/**
-	 * Este método es invocado cuando el usuario desea logearse al 
+	 * Este metodo es invocado cuando el usuario desea logearse al 
 	 * servidor. Se autentica por medio de una credencial provista por
 	 * Darkstar y a apartir de esta se obtiene el nombre y el password
-	 * que serán dados al método {@link #checkPlayer()} para que éste
-	 * verifique si dicha información es válida para autentificarse. 
+	 * que seran dados al metodo {@link #checkPlayer()} para que este
+	 * verifique si dicha informacion es valida para autentificarse. 
 	 * 
-	 * @param credentials para autentificación 
-	 * @return Identity associeted to player.
-	 * @throws CredentialException si la credencial no es válida
+	 * @param credentials para autentificacion. 
+	 * @return Identity asosiada al {@link Player}.
+	 * @throws CredentialException si la credencial no es valida.
 	 */
 	public Identity authenticateIdentity(final IdentityCredentials credentials) 
 	throws CredentialException {
@@ -58,16 +60,16 @@ public final class LoginPasswordAuthenticator implements IdentityAuthenticator {
 
 		NamePasswordCredentials npc = (NamePasswordCredentials) credentials;
 
-		// se obtiene el password y nombre que ingresó el usuario. 
+		// Se obtiene el password y nombre que ingreso el usuario. 
 		String name = npc.getName();
 		char[] passaux = npc.getPassword();
 		String password = new String(passaux);
 
-		// verificar si la información ingresada es válida con la 
-		// registrada en el ModelAccess. 
+		// Verificar si la informacion ingresada es valida, con la 
+		// que esta registrada en el ModelAccess. 
 		boolean isValid = ModelAccess.getInstance().checkPlayer(password, name);
 
-		// si no es válida se rechaza el ingreso al usuario.
+		// Si no es valida se rechaza el ingreso al usuario.
 		if (!isValid) {
 			throw new CredentialException(
 			"El nombre de usuario o contraseña no es válida");
@@ -85,6 +87,7 @@ public final class LoginPasswordAuthenticator implements IdentityAuthenticator {
 	 * @return identificadores para los tipos de credenciales soportados
 	 * por esta clase.
 	 */
+	
 	public String[] getSupportedCredentialTypes() {
 		return new String [] { NamePasswordCredentials.TYPE_IDENTIFIER };
 	}
