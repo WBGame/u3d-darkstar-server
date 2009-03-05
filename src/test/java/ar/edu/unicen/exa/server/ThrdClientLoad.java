65783620c8d4dc62589ca507d964f343e422346e
u3d.testing <u3d.testing@d8cae19c-9ac8-11dd-be93-c58765760c5f>
2009-03-05 22:25:46 +0000

    Mas cambios en el test de carga, aun no es la version estable debido a la variacion en los resultados en distintas ejecuciones con iguales parametros de entrada, el server responde en forma variada.

--- src/test/java/ar/edu/unicen/exa/server/ThrdClientLoad.java
+++ src/test/java/ar/edu/unicen/exa/server/ThrdClientLoad.java
@@ -31,11 +31,34 @@ public class ThrdClientLoad implements Runnable {
 		c.setLogin("ThrdClient" + t.getName());
 		c.setPassword("ThrdClient" + t.getName());
 		c.login();
-		LOGGER.info("ThrdClient nº:" + t.getName() + " Logged");
-		IMessage message = c.buildMessageEnterWorld("201");
-		c.sendMessage(message);
-		message = c.buildMessageMove();
-		c.sendToChannel(message);
+		if (c.isLoggedIn())
+			LOGGER.info("ThrdClient nº:" + t.getName() + " Logged");
+		else
+			LOGGER.info("ThrdClient nº:" + t.getName() + " NOT LOGGED!!");
+		
+		/** Mundos: utilizar los nombres fallo debido a que espera un entero
+		 	buffet = 2
+ 			exa = 5
+ 			exterior = 510
+ 			isistan = 513
+ 			econ = 518
+ 			ac1 = 521
+		   
+		   vamos switcheando entre ambos mundos durante N intentos
+		 * en cada switcheo enviamos un movimiento
+		 * luego nos deslogamos
+		 */
+		String woldId = "5";//exa
+		for (int i = 0; i < 2; i++) {
+			LOGGER.info("ThrdClient nº:" + t.getName() + " cambiando a mundo: " + woldId);
+			IMessage message = c.buildMessageEnterWorld(woldId);
+			c.sendMessage(message);
+			message = c.buildMessageMove();
+			c.sendToChannel(message);
+			woldId = woldId == "513" ? "5" : "513"; //isistan
+		}
+		c.logout();
+		
 	}
 	
 	
